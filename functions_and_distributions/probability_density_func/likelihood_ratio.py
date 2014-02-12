4bff205ad3a0144b9b781e499e7ba48c24171898
rasbt <se.raschka@me.com>
2014-02-12 01:41:35 -0500

    fixed pdf function

--- functions_and_distributions/probability_density_func/likelihood_ratio.py
+++ functions_and_distributions/probability_density_func/likelihood_ratio.py
@@ -6,18 +6,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 
-def pdf(x, mean=0, std_dev=1):
+def pdf(x, mu=0, sigma=1):
     """Calculates the normal distribution's probability density 
         function (PDF).  
         
     """
-    term1 = 1.0 / math.sqrt(2*np.pi) 
-    term2 = np.exp( -0.5 * ( (x-mean)/std_dev )**2 )
+    term1 = 1.0 / ( math.sqrt(2*np.pi) * sigma )
+    term2 = np.exp( -0.5 * ( (x-mu)/sigma )**2 )
     return term1 * term2
 
 x = np.arange(0, 50, 0.05)
 
-likelihood_ratio = pdf(x, mean=30, std_dev=10**0.5) / pdf(x, mean=40, std_dev=20**0.5)
+likelihood_ratio = pdf(x, mu=30, sigma=10**0.5) / pdf(x, mu=40, sigma=20**0.5)
 
 
 plt.plot(x, likelihood_ratio)
--- functions_and_distributions/probability_density_func/probability_density_func.py
+++ functions_and_distributions/probability_density_func/probability_density_func.py
@@ -5,20 +5,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 
-def pdf(x, mean=0, std_dev=1):
+def pdf(x, mu=0, sigma=1):
     """Calculates the normal distribution's probability density 
         function (PDF).  
         
     """
-    term1 = 1.0 / math.sqrt(2*np.pi) 
-    term2 = np.exp( -0.5 * ( (x-mean)/std_dev )**2 )
+    term1 = 1.0 / ( math.sqrt(2*np.pi) * sigma )
+    term2 = np.exp( -0.5 * ( (x-mu)/sigma )**2 )
     return term1 * term2
 
 
 x = np.arange(0, 100, 0.05)
 
-pdf1 = pdf(x, mean=5, std_dev=2.5**0.5)
-pdf2 = pdf(x, mean=10, std_dev=6**0.5)
+pdf1 = pdf(x, mu=5, sigma=2.5**0.5)
+pdf2 = pdf(x, mu=10, sigma=6**0.5)
 
 plt.plot(x, pdf1)
 plt.plot(x, pdf2)
